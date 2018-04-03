81de0456fdd9e5f0acaf292366187183a499d03e
n1k <be.istrefi@gmail.com>
2018-04-03 11:52:36 +0200

    Revert "1.1"
    This reverts commit 4ee6810

--- src/ch/uniteit/yahtzee/gui/GUI.java
+++ src/ch/uniteit/yahtzee/gui/GUI.java
@@ -23,23 +23,12 @@ public class GUI extends JFrame{
 	
 	public GUI() {
 		
-		/*
-		 * Erstellt ein JFrame und setzt den Layoutmanager
-		 */
-		
 		super("Yathzee");
+
 		this.setSize(800,800);
 		this.setLayout(new BorderLayout());
+		
 		this.getContentPane().setBackground(Color.GREEN);
-		
-		// Instanziert neue JPanel 
-		
-		this.table = new JPanel();
-		this.sheetPanel = new JPanel();
-		this.playPanel =  new JPanel();
-		this.userPanel =  new JPanel();
-		
-		
 		
 		this.setVisible(true);
 		
