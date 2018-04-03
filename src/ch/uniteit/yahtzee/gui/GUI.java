bb428110a4589aba478ce615cce5913df253dd09
fernandomaniglio <fernandomaniglio@10.32.105.182>
2018-04-03 12:12:05 +0200

    1.3

--- src/ch/uniteit/yahtzee/gui/GUI.java
+++ src/ch/uniteit/yahtzee/gui/GUI.java
@@ -3,6 +3,8 @@ package ch.uniteit.yahtzee.gui;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 
 import javax.swing.ImageIcon;
@@ -18,7 +20,8 @@ public class GUI extends JFrame{
 	private JPanel table;
 	private JPanel sheetPanel;
 	private JPanel playPanel;
-	private JPanel userPanel;
+	private JPanel userPanel1;
+	private JPanel userPanel2;
 	
 	
 	public GUI() {
@@ -29,7 +32,8 @@ public class GUI extends JFrame{
 		
 		super("Yathzee");
 		this.setSize(800,800);
-		this.setLayout(new BorderLayout());
+		this.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
 		this.getContentPane().setBackground(Color.GREEN);
 		
 		// Instanziert neue JPanel 
@@ -37,11 +41,24 @@ public class GUI extends JFrame{
 		this.table = new JPanel();
 		this.sheetPanel = new JPanel();
 		this.playPanel =  new JPanel();
-		this.userPanel =  new JPanel();
+		this.userPanel1 =  new JPanel();
+		this.userPanel2 = new JPanel();
 		
+
+		initGui();
 		
 		
 		this.setVisible(true);
+		
+	}
+	
+	public void initGui() {
+		
+		this.add(table, BorderLayout.CENTER);
+		this.add(sheetPanel, BorderLayout.EAST);
+		this.add(playPanel, BorderLayout.CENTER);
+		this.add(userPanel1, BorderLayout.NORTH);
+		this.add(userPanel2, BorderLayout.SOUTH);
 		
 	}
 	
