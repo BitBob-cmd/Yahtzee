723ee450e851312664a345d424b43b9afab7fb3d
fernandomaniglio <fernandomaniglio@10.32.105.182>
2018-04-03 11:45:20 +0200

    1.0

--- src/ch/uniteit/yahtzee/gui/GUI.java
+++ src/ch/uniteit/yahtzee/gui/GUI.java
@@ -1,8 +1,37 @@
 package ch.uniteit.yahtzee.gui;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+/*
+ * Diese Klasse ist für die Visualisierung des spieles zuständig
+ * @author Besnik Istrefi & Fernando Maniglio 
+ */
 
 public class GUI extends JFrame{
 	
-	super("Hallo");
+	private JPanel table;
+	private JPanel sheetPanel;
+	private JPanel playPanel;
+	private JPanel userPanel;
+	
+	
+	public GUI() {
+		
+		super("Yathzee");
+
+		this.setSize(800,800);
+		this.setLayout(new BorderLayout());
+		
+		this.getContentPane().setBackground(Color.GREEN);
+		
+		this.setVisible(true);
+		
+	}
+	
 }
