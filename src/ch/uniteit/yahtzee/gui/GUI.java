8ba3636e18aaeb1832a9565a8f0818ce69eb8ec1
n1k <be.istrefi@gmail.com>
2018-04-05 10:08:07 +0200

    Wuerfel ArrayList und das Würfeln in zwei Methoden unterteilt.

--- src/ch/uniteit/yahtzee/gui/GUI.java
+++ src/ch/uniteit/yahtzee/gui/GUI.java
@@ -12,6 +12,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayList;
 import java.util.Random;
 
 import javax.swing.JButton;
@@ -21,7 +22,7 @@ import javax.swing.JTable;
 
 /*
  * Diese Klasse ist für die Visualisierung des spieles zuständig
- * @author Besnik Istrefi & Fernando Maniglio 
+ * @author Besnik Istrefi & Fernando Maniglio
  */
 
 public class GUI extends JFrame implements MouseListener {
@@ -47,15 +48,19 @@ public class GUI extends JFrame implements MouseListener {
 
 	// Dices Attribute
 
-	private Dices[] wuerfelAkt;
+	private ArrayList<Dices> wuerfelAkt;
 
-	
-	
+	// Attribut für Playtabel
 
-	
-	
+	private PlayTable spielTisch;
 
-	public GUI() {
+
+
+
+
+
+
+	public GUI() throws Exception {
 		// Defaultkonstruktor für erst Initialiseriung
 
 		super("Yathzee");
@@ -94,9 +99,13 @@ public class GUI extends JFrame implements MouseListener {
 		this.tabelle = new JTable(20, 4);
 		this.tabelle.setGridColor(Color.black);
 
-		this.wuerfelAkt = new Dices[5];
+		this.wuerfelAkt = new ArrayList<>();
+		this.spielTisch = new PlayTable(5);
+
+
 
 		initGui();
+
 
 		this.setVisible(true);
 
@@ -115,18 +124,25 @@ public class GUI extends JFrame implements MouseListener {
 		panelEast.add(panelWuerfelAktiv, BorderLayout.WEST);
 		panelEast.add(panelWuerfelDeaktiv, BorderLayout.CENTER);
 
+
+
+		deaktivePanelErstellen();
+		aktivePanelErstelle();
+		wuerfelErstellen();
+
 		buttons.add(wuerfeln);
 		buttons.add(neuesSpiel);
 		buttons.add(besteListe);
-		
-		//ActionListener Buttons 
-		
+
+		//ActionListener Buttons
+
 		wuerfeln.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 
-				for(int count = 0 ; count < wuerfelAkt.length ; count++) {
+				for(Dices d : wuerfelAkt) {
+
 					repaint();
 
 				}
@@ -134,15 +150,37 @@ public class GUI extends JFrame implements MouseListener {
 			}
 		});
 
-		for (int count = 0; count < panelWuerfelAktivFlow.length; count++) {
 
-			panelWuerfelAktivFlow[count] = new JPanel();
-			panelWuerfelAktivFlow[count].setLayout(new FlowLayout());
-			panelWuerfelAktivFlow[count].setBackground(Color.GREEN);
-			
-			panelWuerfelAktiv.add(panelWuerfelAktivFlow[count]);
 
-		}
+		neuesSpiel.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+
+
+
+		besteListe.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+	}
+
+
+
+
+	// Diese Methode erstellt neue JPanel für den Deaktiven Bereich der Würfel
+
+	public void deaktivePanelErstellen() {
 
 		for (int count = 0; count < panelWuerfelDeaktivFlow.length; count++) {
 
@@ -154,72 +192,107 @@ public class GUI extends JFrame implements MouseListener {
 
 		}
 
+	}
+
+
+	// Diese Methode erstellt neue JPanel für den Aktiven Bereich der Würfel
+
+	public void aktivePanelErstelle() {
+
+		for (int count = 0; count < panelWuerfelAktivFlow.length; count++) {
+
+			panelWuerfelAktivFlow[count] = new JPanel();
+			panelWuerfelAktivFlow[count].setLayout(new FlowLayout());
+			panelWuerfelAktivFlow[count].setBackground(Color.GREEN);
+
+			panelWuerfelAktiv.add(panelWuerfelAktivFlow[count]);
+
+		}
+
+	}
+
+	// Würfel erstellen und MouseListener Hinzufügen
+
+	public void wuerfelErstellen() {
+
 		int index = 0;
-		while (index < wuerfelAkt.length) {
+		while (index < spielTisch.getWuerfelAnzahl()) {
 
-			wuerfelAkt[index] = new Dices(index);
+			wuerfelAkt.add(new Dices(index));
 
-			panelWuerfelAktivFlow[index].add(wuerfelAkt[index]);
+			panelWuerfelAktivFlow[index].add(wuerfelAkt.get(index));
 
-			wuerfelAkt[index].addMouseListener(this); 
+			wuerfelAkt.get(index).addMouseListener(this);
 
 			index++;
 		}
 
 	}
-	
+
+
+
+
+
+
 	@Override
 	public void mouseClicked(MouseEvent e) {
+
 		Dices x = ((Dices)e.getSource());
+
+
 
 		if(panelWuerfelAktivFlow[x.getFlowIndex()] == x.getParent()) {
 			x.getParent().remove(x);
 			panelWuerfelDeaktivFlow[x.getFlowIndex()].add(x);
-			
+
 		}
 		else if(panelWuerfelDeaktivFlow[x.getFlowIndex()] == x.getParent()) {
 			x.getParent().remove(x);
 			panelWuerfelAktivFlow[x.getFlowIndex()].add(x);
 		}
+
+
+
+
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 
 	/*
 	 * Interne Klasse für die Instanzierung und Zeichnung der Würfel
-	 * 
-	 * 
+	 *
+	 *
 	 */
 
-	
 
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws Exception {
 
 		GUI temp = new GUI();
 	}
-
-	
 }
+
+
