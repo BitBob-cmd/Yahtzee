c5fd25bb3ba55ab6639edbf918598808a4425793
fernandomaniglio <fernandomaniglio@10.32.108.50>
2018-04-05 16:57:44 +0200

    10

--- src/ch/uniteit/yahtzee/logic/Rules.java
+++ src/ch/uniteit/yahtzee/logic/Rules.java
@@ -1,5 +1,7 @@
 package ch.uniteit.yahtzee.logic;
 
+import java.util.ArrayList;
+
 import ch.uniteit.yahtzee.gui.GUI;
 
 public class Rules extends GUI {
@@ -12,7 +14,8 @@ public class Rules extends GUI {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
+	// Attribute für Resultate oben
 	private int einerResultat;
 	private int zweierResultat;
 	private int dreierResultat;
@@ -20,23 +23,58 @@ public class Rules extends GUI {
 	private int fuenferResultat;
 	private int secherResultat;
 	private int summeOben;
+
+	// Attribute für Resultate unten
+
+	private int dreierPaschResultat;
+	private int viererPaschResultat;
+	private int fullHouseResultat;
+	private int kleineStrasseResultat;
+	private int grosseStrasseResultat;
 	
+	private int yathzeeKniffelResultat;
+	private boolean isYathzeeTwice;
+	
+	private int chanceResulat;
+	private int summeUnten;
 
-	public Rules() throws Exception{
+	// Attribute counter für Dreier und Viererpasch
 
-		
+	private int counterWuerfelOnHoldDreierpasch;
+	private int counterWuerfelOnHoldViererpasch;
+	
+	//Attribut für Gesamtsumme und Bonus
+	
+	private int gesamtsumme;
+	private int bonus;
+
+	public Rules() throws Exception {
+
 		this.einerResultat = 0;
 		this.zweierResultat = 0;
 		this.dreierResultat = 0;
 		this.viererResultat = 0;
 		this.fuenferResultat = 0;
 		this.secherResultat = 0;
-		this.summeOben = 0 ;
-		
+		this.summeOben = 0;
+		this.dreierPaschResultat = 0;
+		this.viererPaschResultat = 0;
+		this.fullHouseResultat = 0;
+		this.kleineStrasseResultat = 0;
+		this.grosseStrasseResultat = 0;
+		this.yathzeeKniffelResultat = 0;
+		this.isYathzeeTwice = false;
+		this.gesamtsumme = 0;
+		this.bonus = 0;
+		this.chanceResulat = 0;
+		this.summeUnten = 0;
+		this.counterWuerfelOnHoldDreierpasch = 0;
+		this.counterWuerfelOnHoldViererpasch = 0;
+
 	}
-	
+
 	// Prüft ob die ausgewählten Würfel 1ner sind.
-	
+
 	public boolean einerPruefung() {
 
 		boolean x = false;
@@ -51,9 +89,9 @@ public class Rules extends GUI {
 		}
 		return x;
 	}
-	
+
 	// Liefert die Summer aller aktiven Würfel zurück nach der Prüfung.
-	
+
 	public int einerResultat() {
 
 		int resultat = 0;
@@ -72,7 +110,7 @@ public class Rules extends GUI {
 	}
 
 	// Prüft ob die ausgewählten Würfel 2er sind.
-	
+
 	public boolean zweierPruefung() {
 
 		boolean x = false;
@@ -89,7 +127,7 @@ public class Rules extends GUI {
 	}
 
 	// Liefert die Summer aller aktiven Würfel zurück nach der Prüfung.
-	
+
 	public int zweierResultat() {
 
 		int resultat = 0;
@@ -106,9 +144,9 @@ public class Rules extends GUI {
 		}
 		return this.zweierResultat;
 	}
-	
+
 	// Prüft ob die ausgewählten Würfel 3er sind.
-	
+
 	public boolean dreierPruefung() {
 
 		boolean x = false;
@@ -125,7 +163,7 @@ public class Rules extends GUI {
 	}
 
 	// Liefert die Summer aller aktiven Würfel zurück nach der Prüfung.
-	
+
 	public int dreierResultat() {
 
 		int resultat = 0;
@@ -142,9 +180,9 @@ public class Rules extends GUI {
 		}
 		return this.dreierResultat;
 	}
-	
+
 	// Prüft ob die ausgewählten Würfel 4er sind.
-	
+
 	public boolean viererPruefung() {
 
 		boolean x = false;
@@ -161,7 +199,7 @@ public class Rules extends GUI {
 	}
 
 	// Liefert die Summer aller aktiven Würfel zurück nach der Prüfung.
-	
+
 	public int viererResultat() {
 
 		int resultat = 0;
@@ -178,9 +216,9 @@ public class Rules extends GUI {
 		}
 		return this.viererResultat;
 	}
-	
+
 	// Prüft ob die ausgewählten Würfel 5er sind.
-	
+
 	public boolean fuenferPruefung() {
 
 		boolean x = false;
@@ -197,7 +235,7 @@ public class Rules extends GUI {
 	}
 
 	// Liefert die Summer aller aktiven Würfel zurück nach der Prüfung.
-	
+
 	public int fuenferResultat() {
 
 		int resultat = 0;
@@ -214,9 +252,9 @@ public class Rules extends GUI {
 		}
 		return this.fuenferResultat;
 	}
-	
+
 	// Prüft ob die ausgewählten Würfel 6er sind.
-	
+
 	public boolean sechserPruefung() {
 
 		boolean x = false;
@@ -233,7 +271,7 @@ public class Rules extends GUI {
 	}
 
 	// Liefert die Summer aller aktiven Würfel zurück nach der Prüfung.
-	
+
 	public int sechserResultat() {
 
 		int resultat = 0;
@@ -250,43 +288,390 @@ public class Rules extends GUI {
 		}
 		return this.secherResultat;
 	}
-	
-	// Summe oben addiert alle Resultate von 1-6 und liefert das Resultat als Integer zurück
-	
+
+	// Summe oben addiert alle Resultate von 1-6 und liefert das Resultat als
+	// Integer zurück
+
 	public int summeOben() {
-		
-		this.summeOben = einerResultat+zweierResultat+dreierResultat+viererResultat+fuenferResultat+secherResultat;
+
+		this.summeOben = einerResultat + zweierResultat + dreierResultat + viererResultat + fuenferResultat
+				+ secherResultat;
 		return summeOben;
 	}
 	
-	// Methode für den Bonus falls die Summe aller Resultate von 1-6 grösser ist als 63 returniert sie 35 Punkte Bonus;
-	public int bonus() {
+	// Summe unten addiert alle Resultate ab dreierpasch und liefert das Resultat als Integer zurück
+	
+	public int summeUnten() {
 		
-		if(this.summeOben >=63) {
+		this.summeUnten = dreierPaschResultat+viererPaschResultat+fullHouseResultat+kleineStrasseResultat+grosseStrasseResultat+
+				yathzeeKniffelResultat+chanceResulat;
+		bonus();
+		return this.summeUnten;
+		
+	}
+
+	// Methode für den Bonus falls die Summe aller Resultate von 1-6 grösser ist als
+	// 63 returniert sie 35 Punkte Bonus;
+	public int bonus() {
+
+		if (this.summeOben >= 63) {
 			
+			this.bonus = 35;
 			return 35;
 		}
-		return 0;	
-		}
-	
-	
+		return 0;
+	}
+
 	// Prüft ob es sich um ein Dreierpasch handelt.
-	
-	public boolean dreierPasch() {
-		
+
+	public boolean dreierPaschPruefung() {
+
 		boolean x = false;
 
 		for (Dices d : getSpielTisch().gibWuerfel()) {
 
-			if (d.getRollScore() == 6 && d.getOnHold() == true) {
+			int tempScore;
 
-				x = true;
+			if (d.getOnHold() == true) {
+				counterWuerfelOnHoldDreierpasch++;
+				tempScore = d.getRollScore();
+
+				if (counterWuerfelOnHoldDreierpasch == 3 && d.getRollScore() == tempScore) {
+
+					x = true;
+				}
+
 			}
 
 		}
 		return x;
 	}
+
+	// Liefert das Resultat des DreierPaschs
+
+	public int ResultatdreierPasch() {
+
+		int resultat = 0;
+
+		if (dreierPaschPruefung() == true) {
+
+			for (Dices d : getSpielTisch().gibWuerfel()) {
+
+				if (d.getOnHold() == true) {
+
+					this.dreierPaschResultat = resultat + d.getRollScore();
+				}
+			}
+		}
+		return this.dreierPaschResultat;
+	}
+
+	// Prüft ob es sich um ein Viererpasch handelt.
+
+	public boolean viererPaschPruefung() {
+
+		boolean x = false;
+
+		for (Dices d : getSpielTisch().gibWuerfel()) {
+
+			int tempScore;
+
+			if (d.getOnHold() == true) {
+				counterWuerfelOnHoldViererpasch++;
+				tempScore = d.getRollScore();
+
+				if (counterWuerfelOnHoldViererpasch == 4 && d.getRollScore() == tempScore) {
+
+					x = true;
+				}
+
+			}
+
+		}
+		return x;
+	}
+
+	// Liefert das Resultat des Viererpaschs
+
+	public int ResultatViererPasch() {
+
+		int resultat = 0;
+
+		if (viererPaschPruefung() == true) {
+
+			for (Dices d : getSpielTisch().gibWuerfel()) {
+
+				if (d.getOnHold() == true) {
+
+					this.viererPaschResultat = resultat + d.getRollScore();
+				}
+			}
+		}
+		return this.viererPaschResultat;
+	}
+
+	// Prüft ob es sich um ein Full House handelt
+
+	public boolean fullHousePruefung() {
+
+		boolean x = false;
+
+		int[] temp = new int[6];
+
+		for (Dices d : getSpielTisch().gibWuerfel()) {
+
+			if (d.getOnHold() == true) {
+				int s = d.getRollScore();
+
+				temp[s]++;
+
+			}
+
+		}
+
+		boolean zweier = false;
+		boolean dreier = false;
+
+		for (int i : temp) {
+
+			if (i == 2) {
+				zweier = true;
+			}
+
+			if (i == 3) {
+
+				dreier = true;
+			}
+
+		}
+
+		if (zweier == true && dreier == true) {
+
+			x = true;
+		}
+
+		return x;
+	}
+
+	// Liefert das Resultat des Fullhouses
+
+	public int fullhouseResultat() {
+
+		if (fullHousePruefung() == true) {
+
+			this.fullHouseResultat = 25;
+		}
+
+		return this.fullHouseResultat;
+	}
+
+	// Prüft ob es sich um eine Kleine Strasse handelt
+
+	public boolean kleineStrassePruefung() {
+
+		boolean x = false;
+
+		int[] temp = new int[6];
+
+		for (Dices d : getSpielTisch().gibWuerfel()) {
+
+			if (d.getOnHold() == true) {
+				int s = d.getRollScore();
+
+				temp[s]++;
+
+			}
+
+		}
+
+		boolean einer = false;
+		boolean zweier = false;
+		boolean dreier = false;
+		boolean vierer = false;
+		boolean fuenfer = false;
+		boolean sechser = false;
+
+		for (int i : temp) {
+
+			if (i == 1 && i == 2 && i == 3 && i == 4) {
+
+				einer = true;
+				zweier = true;
+				dreier = true;
+				vierer = true;
+
+			}
+			if (i == 2 && i == 3 && i == 4 && i == 5) {
+
+				zweier = true;
+				dreier = true;
+				vierer = true;
+				fuenfer = true;
+
+			}
+
+			if (i == 3 && i == 4 && i == 5 && i == 6) {
+
+				dreier = true;
+				vierer = true;
+				fuenfer = true;
+				sechser = true;
+			}
+
+		}
+		if (einer == true && zweier == true && dreier == true && vierer == true
+				|| zweier == true && dreier == true && vierer == true && fuenfer == true
+				|| dreier == true && vierer == true && fuenfer == true && sechser == true) {
+
+			x = true;
+		}
+
+		return x;
+	}
+
+	// Liefert das Resultat des Fullhouses
+
+	public int kleineStrasseResultat() {
+
+		if (kleineStrassePruefung() == true) {
+
+			this.kleineStrasseResultat = 30;
+		}
+
+		return this.kleineStrasseResultat;
+	}
+
+	// Prüft ob es sich um eine Grosse Strasse handelt
+
+	public boolean grosseStrassePruefung() {
+
+		boolean x = false;
+
+		int[] temp = new int[6];
+
+		for (Dices d : getSpielTisch().gibWuerfel()) {
+
+			if (d.getOnHold() == true) {
+				int s = d.getRollScore();
+
+				temp[s]++;
+
+			}
+
+		}
+
+		boolean einer = false;
+		boolean zweier = false;
+		boolean dreier = false;
+		boolean vierer = false;
+		boolean fuenfer = false;
+		boolean sechser = false;
+
+		for (int i : temp) {
+
+			if (i == 1 && i == 2 && i == 3 && i == 4 && i == 5) {
+
+				einer = true;
+				zweier = true;
+				dreier = true;
+				vierer = true;
+				fuenfer = true;
+
+			}
+			if (i == 2 && i == 3 && i == 4 && i == 5 && i == 6) {
+
+				zweier = true;
+				dreier = true;
+				vierer = true;
+				fuenfer = true;
+				sechser = true;
+
+			}
+
+		}
+		if (einer == true && zweier == true && dreier == true && vierer == true && fuenfer == true
+				|| zweier == true && dreier == true && vierer == true && fuenfer == true && sechser == true) {
+
+			x = true;
+		}
+
+		return x;
+	}
+
+	// Liefert das Resultat der grossen Strasse
+
+	public int grosseStrasseResultat() {
+
+		if (grosseStrassePruefung() == true) {
+
+			this.grosseStrasseResultat = 40;
+		}
+
+		return this.grosseStrasseResultat;
+	}
+
+	// Prüft ob es sich um ein Yathzee/Kniffel handelt
+	
+	public boolean yathzeeKniffelPruefung() {
+
+		boolean x = true;
+		int tempDice = getSpielTisch().gibWuerfel().get(0).getRollScore();
 		
+		for (Dices d : getSpielTisch().gibWuerfel()) {
+			
+			if (d.getOnHold() == true) {
+				
+				if (d.getRollScore() != tempDice) {
+
+					x = false;
+				}
+
+			}
+
+		}
+		return x;
+	}
+
+	// Liefert das Resultat des Yathzee/Kniffel
+	
+	public int yathzeeKniffelResultat() {
+		
+		 if(yathzeeKniffelPruefung() == true && isYathzeeTwice == false) {
+			 
+			 this.isYathzeeTwice = true;
+			 this.yathzeeKniffelResultat = 50;
+		 }
+		
+		 else {
+			 
+			 this.yathzeeKniffelResultat = yathzeeKniffelResultat+100;
+		 }
+		 
+		 return this.yathzeeKniffelResultat;
+	}
+	
+	// Liefert das Resultat der Chance
+
+	public int ResultatChance() {
+
+		int resultat = 0;
+
+		for (Dices d : getSpielTisch().gibWuerfel()) {
+
+			this.chanceResulat = resultat + d.getRollScore();
+
+		}
+		return this.chanceResulat;
+	}
+	
+	// Liefert die gesamtsumme Oben Bonus und Unten als Integer zurück
+	
+	public int gesamtsumme() {
+		
+		this.gesamtsumme = summeOben+summeUnten+bonus;
+		return this.gesamtsumme;
+	}
+	
 	
 
 }
